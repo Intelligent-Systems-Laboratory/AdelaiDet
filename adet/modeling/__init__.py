d0cd71f69857b5a7f0b6880a614a428ff2ea1456
johnanthonyjose <john.anthony.jose@dlsu.edu.ph>
2020-11-10 23:06:58 -0500

    Add effnet on init

--- adet/modeling/__init__.py
+++ adet/modeling/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from .fcos import FCOS
 from .blendmask import BlendMask
-from .backbone import build_fcos_resnet_fpn_backbone
+from .backbone import build_fcos_resnet_fpn_backbone, build_effnet_backbone
 from .one_stage_detector import OneStageDetector, OneStageRCNN
 from .roi_heads.text_head import TextHead
 from .batext import BAText
