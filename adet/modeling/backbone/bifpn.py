b20e9faeb5a5c8f7de5352ceb9cdd8e56b45b908
johnanthonyjose <john.anthony.jose@dlsu.edu.ph>
2020-11-11 04:35:35 -0500

    Add RetinaNet EffNet FPN

--- adet/modeling/backbone/bifpn.py
+++ adet/modeling/backbone/bifpn.py
@@ -4,7 +4,7 @@ from torch import nn
 
 from detectron2.layers import Conv2d, ShapeSpec, get_norm
 
-from detectron2.modeling.backbone import Backbone, build_resnet_backbone
+from detectron2.modeling.backbone import Backbone, build_resnet_backbone, build_effnet_backbone
 from detectron2.modeling import BACKBONE_REGISTRY
 from .mobilenet import build_mnv2_backbone
 
@@ -385,6 +385,31 @@ def build_fcos_resnet_bifpn_backbone(cfg, input_shape: ShapeSpec):
     out_channels = cfg.MODEL.BiFPN.OUT_CHANNELS
     num_repeats = cfg.MODEL.BiFPN.NUM_REPEATS
     top_levels = cfg.MODEL.FCOS.TOP_LEVELS
+
+    backbone = BiFPN(
+        bottom_up=bottom_up,
+        in_features=in_features,
+        out_channels=out_channels,
+        num_top_levels=top_levels,
+        num_repeats=num_repeats,
+        norm=cfg.MODEL.BiFPN.NORM
+    )
+    return backbone
+
+
+@BACKBONE_REGISTRY.register()
+def build_retinanet_effnet_bifpn_backbone(cfg, input_shape: ShapeSpec):
+    """
+    Args:
+        cfg: a detectron2 CfgNode
+    Returns:
+        backbone (Backbone): backbone module, must be a subclass of :class:`Backbone`.
+    """
+    bottom_up = build_effnet_backbone(cfg, input_shape)
+    in_features = cfg.MODEL.BiFPN.IN_FEATURES
+    out_channels = cfg.MODEL.BiFPN.OUT_CHANNELS
+    num_repeats = cfg.MODEL.BiFPN.NUM_REPEATS
+    top_levels = 0
 
     backbone = BiFPN(
         bottom_up=bottom_up,
--- adet/modeling/backbone/fpn.py
+++ adet/modeling/backbone/fpn.py
@@ -2,14 +2,14 @@ from torch import nn
 import torch.nn.functional as F
 import fvcore.nn.weight_init as weight_init
 
-from detectron2.modeling.backbone import FPN, build_resnet_backbone
+from detectron2.modeling.backbone import FPN, LastLevelMaxPool, build_resnet_backbone
 from detectron2.layers import ShapeSpec
 from detectron2.modeling.backbone.build import BACKBONE_REGISTRY
 
 from .resnet_lpf import build_resnet_lpf_backbone
 from .resnet_interval import build_resnet_interval_backbone
 from .mobilenet import build_mnv2_backbone
-
+from .effnet import build_effnet_backbone
 
 class LastLevelP6P7(nn.Module):
     """
@@ -83,6 +83,51 @@ def build_fcos_resnet_fpn_backbone(cfg, input_shape: ShapeSpec):
         out_channels=out_channels,
         norm=cfg.MODEL.FPN.NORM,
         top_block=top_block,
+        fuse_type=cfg.MODEL.FPN.FUSE_TYPE,
+    )
+    return backbone
+
+
+@BACKBONE_REGISTRY.register()
+def build_effnet_fpn_backbone(cfg, input_shape: ShapeSpec):
+    """builds efficienet with FPN
+    Args:
+        cfg: a detectron2 CfgNode
+
+    Returns:
+        backbone (Backbone): backbone module, must be a subclass of :class:`Backbone`.
+    """
+    bottom_up = build_effnet_backbone(cfg, input_shape)
+    in_features = cfg.MODEL.FPN.IN_FEATURES
+    out_channels = cfg.MODEL.FPN.OUT_CHANNELS
+    backbone = FPN(
+        bottom_up=bottom_up,
+        in_features=in_features,
+        out_channels=out_channels,
+        norm=cfg.MODEL.FPN.NORM,
+        top_block=LastLevelMaxPool(),
+        fuse_type=cfg.MODEL.FPN.FUSE_TYPE,
+    )
+    return backbone
+
+@BACKBONE_REGISTRY.register()
+def build_retinanet_effnet_fpn_backbone(cfg, input_shape: ShapeSpec):
+    """
+    Args:
+        cfg: a detectron2 CfgNode
+    Returns:
+        backbone (Backbone): backbone module, must be a subclass of :class:`Backbone`.
+    """
+    bottom_up = build_effnet_backbone(cfg, input_shape)
+    in_features = cfg.MODEL.FPN.IN_FEATURES
+    out_channels = cfg.MODEL.FPN.OUT_CHANNELS
+    in_channels_p6p7 = bottom_up.output_shape()["s7"].channels
+    backbone = FPN(
+        bottom_up=bottom_up,
+        in_features=in_features,
+        out_channels=out_channels,
+        norm=cfg.MODEL.FPN.NORM,
+        top_block=LastLevelP6P7(in_channels_p6p7, out_channels),
         fuse_type=cfg.MODEL.FPN.FUSE_TYPE,
     )
     return backbone
