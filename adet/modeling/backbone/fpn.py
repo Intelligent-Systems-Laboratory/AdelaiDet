ee0fbeabcfe096e72f7f0c9c958df22226a287db
johnanthonyjose <john.anthony.jose@dlsu.edu.ph>
2020-11-13 19:01:14 +0000

    Add the winning config file

--- adet/modeling/backbone/fpn.py
+++ adet/modeling/backbone/fpn.py
@@ -103,7 +103,7 @@ def build_fcos_resnet_fpn_backbone(cfg, input_shape: ShapeSpec):
 
 
 @BACKBONE_REGISTRY.register()
-def build_effnet_fpn_backbone(cfg, input_shape: ShapeSpec):
+def build_fcos_effnet_fpn_backbone(cfg, input_shape: ShapeSpec):
     """builds efficienet with FPN
     Args:
         cfg: a detectron2 CfgNode
@@ -114,12 +114,13 @@ def build_effnet_fpn_backbone(cfg, input_shape: ShapeSpec):
     bottom_up = build_effnet_backbone(cfg, input_shape)
     in_features = cfg.MODEL.FPN.IN_FEATURES
     out_channels = cfg.MODEL.FPN.OUT_CHANNELS
+    in_channels_top = out_channels
     backbone = FPN(
         bottom_up=bottom_up,
         in_features=in_features,
         out_channels=out_channels,
         norm=cfg.MODEL.FPN.NORM,
-        top_block=LastLevelMaxPool(),
+        top_block=LastLevelP6P7(in_channels_top, out_channels, "p5"),
         fuse_type=cfg.MODEL.FPN.FUSE_TYPE,
     )
     return backbone
